c9143aeab14b6b38581499ba62f10585a1c53607
Dominika Barrett <barrettdomi@gmail.com>
2021-01-07 21:50:34 +0000

    fixing upadtes board and and confetti.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const playerScoreEl = document.getElementById('playerScore');
+const playerScoreEl= document.getElementById('playerScore');
 const playerChoiceEl = document.getElementById('playerChoice');
 const computerScoreEl = document.getElementById('computerScore');
 const computerChoiceEl = document.getElementById('computerChoice');
@@ -15,14 +15,15 @@ const computerScissors = document.getElementById('computerScissors');
 const allGameIcons = document.querySelectorAll('.far');
 
 const choices = {
-  rock: { name: 'Rock', defeats: 'scissors' },
-  paper: { name: 'Paper', defeats: 'rock'},
-  scissors: { name: 'Scissors', defeats: 'paper' },
+  rock: { name: 'Rock', defeats:['scissors','scissors']},
+  paper: { name: 'Paper', defeats: ['rock','rock']},
+  scissors: { name: 'Scissors', defeats: ['paper','paper']},
 };
 
 // zmienne globalne
+
 let computerScoreNumber = 0;
-let palyerScoreNumber = 0;
+let playerScoreNumber = 0;
 let computerChoice = '';
 
 // reset all 'seleceted' icons
@@ -33,21 +34,34 @@ function resetSelected() {
   });
 }
 
+// Reset Scaore and palyer and computer choice
+function resetAll() {
+  playerScoreNumber = 0;
+  computerScoreNumber = 0;
+  playerScoreEl.textContent = computerScoreNumber;
+  computerScoreEl.textContent = computerScoreNumber;
+  playerChoiceEl.textContent = '';
+  computerChoiceEl.textContent = '';
+  resultText.textContent = '';
+  resetSelected();
+}
 // compuetr choice
 function computerRandomChoice() {
   const computerChoiceNumber = Math.random();
 //  console.log('computer choice', computerChoiceNumber);
   if (computerChoiceNumber < 0.4) {
     computerChoice = 'rock';
-  } else if (computerChoiceNumber <= 0.6) {
+  } else if (computerChoiceNumber <= 0.7) {
     computerChoice = 'paper';
   } else {
     computerChoice = 'scissors';
   }
-  // console.log(computerChoice);
+  console.log('wybor komputera',computerChoice);
 }
 
-// add seleceted styling and computer choice 
+
+
+//add seleceted styling and computer choice 
 function displayComputerChoice() {
   switch (computerChoice) {
     case 'rock':
@@ -67,36 +81,39 @@ function displayComputerChoice() {
   }
 }
 
-// check resoluts score
+
+
+// check resluts score dodanie do tablicy wyniku i textu  do tabllicy
 function updateScore(playerChoice) {
-  // console.log(playerChoice, computerChoice);
+  console.log(playerChoice, computerChoice);
   if (playerChoice === computerChoice) {
-    resultText.textContent ='Its a tie!'
+    console.log('pokaz', playerChoice, computerChoice);
+    resultText.textContent = 'Its a tie!';
   } else {
     const choice = choices[playerChoice];
     console.log(choice.defeats.indexOf(computerChoice));
     if (choice.defeats.indexOf(computerChoice) > -1) {
       resultText.textContent = 'you won';
-      palyerScoreNumber++;
-      playerScoreEl.textContent = palyerScoreNumber;
+      playerScoreNumber++;
+      playerScoreEl.textContent = playerScoreNumber;
     } else {
-      resultText.textContent = 'You lost'
+      resultText.textContent = 'You lost';
       computerScoreNumber++;
       computerScoreEl.textContent = computerScoreNumber;
-
     }
   }
-
 }
 
+  
 
 
-// call function for turn
+
+// call function for turns
 function checkResult(playerChoice) {
   resetSelected();
   computerRandomChoice();
   displayComputerChoice();
-  updateScore(playerChoice)
+  updateScore(playerChoice);
 }
 
 // passing player onclick
@@ -120,4 +137,9 @@ function select(playerChoice) {
     default:
       break;
   }
+  console.log('wybor playera', playerChoice);
 }
+
+
+// start game
+resetAll();
